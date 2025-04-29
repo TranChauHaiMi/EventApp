f2d8c2c7d59548671f4312db65cdab18865019af
TranChauHaiMi <129152810+TranChauHaiMi@users.noreply.github.com>
2025-04-29 21:19:55 +0700

    Edit Clerk account_3

--- app/layout.tsx
+++ app/layout.tsx
@@ -132,6 +132,7 @@ export default function RootLayout({
             <SyncUserWithConvex />
             {children}
 
+
             <Toaster />
           </ClerkProvider>
         </ConvexClientProvider>
